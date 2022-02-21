4761f48ff21b93f4d737541d64dd8f42970b1b9f
Miquel Canyelles <miqeelcn@gmail.com>
2022-02-21 11:16:55 +0100

    Modified the setup.py adding descriptions, upgrading the version and adding other meta-data

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README", 'r') as f:
+    long_description = f.read()
+
+
 setup(
         name='RCBS.py',
-        version='0.0a0',
+        version='0.0',
+        description='Python package useful for analysing MD trajectories and creating QM/MM models built on top of MDAnalysis. All MD formats compatible with MDAnalysis are compatible with RCBS, while QM/MM capacities are compatible with ChemShell. The aim of this package is to develop an easy-to-use way to analyse MD trajectories, while keeping a good performance.',
+        long_description=long_description,
+        author='Miquel Canyelles Niño',
+        author_mail='mcanyellesnino@gmail.com',
         packages= find_packages(),
-        include_package_data=True)
+        include_package_data=True,
+        install_requires=['numpy', 'pyyaml', 'MDAnalysis'])
 
