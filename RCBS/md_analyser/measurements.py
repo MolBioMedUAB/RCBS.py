5a125ff97ddc5cfb969d4a5de7e31c78dfdd1a41
Miquel Canyelles <miqeelcn@gmail.com>
2022-11-21 12:51:10 +0100

    Removed the prototype for RMSD and adapted for new version upload

--- RCBS/md_analyser/measurements.py
+++ RCBS/md_analyser/measurements.py
@@ -12,23 +12,6 @@ from numpy import min as npmin
 from numpy import array
 
 
-""" TO INCLUDE
-sel       = u.select_atoms('backbone')
-selref    = ref.select_atoms('backbone')
-
-rmsd_test = []
-
-for ts in u.trajectory:
-
-    rmsd_test.append(
-        rms.rmsd(
-            sel.positions - sel.center_of_mass(),
-            selref.positions - selref.center_of_mass(),
-        )        
-    )
-"""
-
-
 class Measurements:
     """
     DESCRIPTION:
