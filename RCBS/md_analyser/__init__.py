6487ebdbf87c7542755f04bc55f5c5c8fce6af55
mikicanyelles <mcanyellesnino@gmail.com>
2022-12-15 15:38:46 +0100

    Added the loading of calculator.py

--- RCBS/md_analyser/__init__.py
+++ RCBS/md_analyser/__init__.py
@@ -1,5 +1,6 @@
 from RCBS.md_analyser import measurements
 from RCBS.md_analyser import selections
 from RCBS.md_analyser import frame_extractor
+from RCBS.md_analyser import calculators
 
 # from RCBS.md_analyser import exceptions
